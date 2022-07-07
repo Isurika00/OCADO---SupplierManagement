d7902d3a4b0b84ae062dfc9e5c48dcaf65b2d6f5
Ashani Isurika <87435558+Isurika00@users.noreply.github.com>
2022-07-07 08:17:43 +0530

    upload commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter} from 'react-router-dom';
+import Header from './Header';
+
+const App = () => (
+ <BrowserRouter>
+	<Header/>
+ </BrowserRouter>	
+
+);
+
+export default App;
---
+++ client/src/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  //views
+  const showNavigation = () => {
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" href="#">Navbar</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="#">Features</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="#">Pricing</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link disabled" href="#">Disabled</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+  };
+
+
+  //render
+  return <header id="header">{showNavigation()}</header>;
+
+};
+
+export default Header;
